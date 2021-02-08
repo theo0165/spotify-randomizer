c73384740f95a28011d1d2a6347e4c1dbb80154a
Theo Sandell <theo01sandell@gmail.com>
2021-02-08 19:47:24 +0100

    Update Spotify.js

--- helpers/Spotify.js
+++ helpers/Spotify.js
@@ -1,7 +1,7 @@
 const spotifyWebApi = require('spotify-web-api-node');
 
 module.exports = new spotifyWebApi({
-    clientId: "f3a8067294d24a3abb5c448d3bede841",
-    clientSecret: "5d910367c1b94fe0af1ccfadd70b2423",
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
     redirectUri: "http://localhost/callback/"
 })
